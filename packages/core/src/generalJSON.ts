9fcd379115d10591d74bef4f5551e839417a4f5c
Lukas.J.Han <lukas.j.han@gmail.com>
2024-11-11 13:12:12 +0900

    feat: add gernealJSON
    Signed-off-by: Lukas.J.Han <lukas.j.han@gmail.com>

---
+++ packages/core/src/generalJSON.ts
@@ -0,0 +1,67 @@
+import { SDJWTException } from '@sd-jwt/utils';
+import { splitSdJwt } from '@sd-jwt/decode';
+
+export type GeneralJSONData = {
+  payload: string;
+  signatures: Array<{
+    protected: string;
+    signature: string;
+    disclosures: Array<string>;
+    kid?: string;
+    kb_jwt?: string;
+  }>;
+};
+
+export class GeneralJSON {
+  public payload: string;
+  public signatures: Array<{
+    protected: string;
+    signature: string;
+    disclosures: Array<string>;
+    kid?: string;
+    kb_jwt?: string;
+  }>;
+
+  constructor(data: GeneralJSONData) {
+    this.payload = data.payload;
+    this.signatures = data.signatures;
+  }
+
+  public static fromEncode(encodedSdJwt: string) {
+    const { jwt, disclosures, kbJwt } = splitSdJwt(encodedSdJwt);
+
+    const { 0: protectedHeader, 1: payload, 2: signature } = jwt.split('.');
+    if (protectedHeader || payload || signature) {
+      throw new SDJWTException('Invalid JWT');
+    }
+
+    return new GeneralJSON({
+      payload,
+      signatures: [
+        {
+          protected: protectedHeader,
+          signature,
+          disclosures,
+          kb_jwt: kbJwt,
+        },
+      ],
+    });
+  }
+
+  public toJson() {
+    return {
+      payload: this.payload,
+      signatures: this.signatures.map((s) => {
+        return {
+          headers: {
+            disclosures: s.disclosures,
+            kid: s.kid,
+            kb_jwt: s.kb_jwt,
+          },
+          protected: s.protected,
+          signature: s.signature,
+        };
+      }),
+    };
+  }
+}
