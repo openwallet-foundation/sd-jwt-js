2f8ed37b716968e21034052b42d87f6103815ab0
Mirko Mollik <mirkomollik@gmail.com>
2025-01-15 13:54:16 +0100

    chore: remove empty line
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>

--- packages/types/src/type.ts
+++ packages/types/src/type.ts
@@ -32,7 +32,6 @@ export const IANA_HASH_ALGORITHMS = [
 
 export type HashAlgorithm = (typeof IANA_HASH_ALGORITHMS)[number];
 
-
 export type SDJWTConfig = {
   omitTyp?: boolean;
   hasher?: Hasher;
@@ -171,15 +170,15 @@ type Frame<Payload> = Payload extends Array<infer U>
     ? Record<number, Frame<U>> & SD<Payload> & DECOY
     : SD<Payload> & DECOY
   : Payload extends Record<string, unknown>
-    ? NonNever<
-        {
-          [K in keyof Payload]?: Payload[K] extends object
-            ? Frame<Payload[K]>
-            : never;
-        } & SD<Payload> &
-          DECOY
-      >
-    : SD<Payload> & DECOY;
+  ? NonNever<
+      {
+        [K in keyof Payload]?: Payload[K] extends object
+          ? Frame<Payload[K]>
+          : never;
+      } & SD<Payload> &
+        DECOY
+    >
+  : SD<Payload> & DECOY;
 
 /**
  * This is a disclosureFrame type that is used to represent the structure of what is being disclosed.
