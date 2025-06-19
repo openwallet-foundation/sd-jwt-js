a2b0b9aecbc12d8b177b5fd97e14813883904ebf
Pascal Knoth <pascal@malach.it>
2025-06-19 21:19:03 +0200

    chore: WIP expose @sd-jwt/sd-jwt-vc public types
    Signed-off-by: Pascal Knoth <pascal@malach.it>

---
+++ packages/sd-jwt-vc/src/types.ts
@@ -0,0 +1,5 @@
+export { SDJWTVCConfig } from './sd-jwt-vc-config';
+export { SDJWTVCStatusReference } from './sd-jwt-vc-status-reference';
+export { SdJwtVcPayload } from './sd-jwt-vc-payload';
+export { TypeMetadataFormat } from './sd-jwt-vc-type-metadata-format';
+export { VcTFetcher } from './sd-jwt-vc-vct';
