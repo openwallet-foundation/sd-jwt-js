1b656ac21796b715096feae439de6b48442244a9
Mirko Mollik <mirkomollik@gmail.com>
2025-06-20 10:47:52 +0200

    fix: expose types (#290)
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- packages/sd-jwt-vc/src/index.ts
+++ packages/sd-jwt-vc/src/index.ts
@@ -2,3 +2,6 @@ export * from './sd-jwt-vc-config';
 export * from './sd-jwt-vc-instance';
 export * from './sd-jwt-vc-payload';
 export * from './sd-jwt-vc-status-reference';
+export * from './sd-jwt-vc-type-metadata-format';
+export * from './sd-jwt-vc-vct';
+export * from './verification-result';
