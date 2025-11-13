ad7dd208230f93a5987ef559b724d1d89abb023f
Mirko Mollik <mirko.mollik@eudi.sprind.org>
2025-11-13 10:19:18 +0100

    chore: format
    Signed-off-by: Mirko Mollik <mirko.mollik@eudi.sprind.org>

--- packages/jwt-status-list/src/status-list-exception.ts
+++ packages/jwt-status-list/src/status-list-exception.ts
@@ -2,7 +2,7 @@
  * SLException is a custom error class for Status List related exceptions.
  */
 export declare class SLException extends Error {
-    details?: unknown;
-    constructor(message: string, details?: unknown);
-    getFullMessage(): string;
+  details?: unknown;
+  constructor(message: string, details?: unknown);
+  getFullMessage(): string;
 }
