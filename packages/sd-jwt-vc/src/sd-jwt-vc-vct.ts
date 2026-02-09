2b4da30b813795b6a4e73989393084841738d186
Berend Sliedrecht <berend@animo.id>
2026-02-09 10:30:32 +0100

    chore: rename VcTFetcher to VCTFetcher
    Signed-off-by: Berend Sliedrecht <berend@animo.id>

--- packages/sd-jwt-vc/src/sd-jwt-vc-vct.ts
+++ packages/sd-jwt-vc/src/sd-jwt-vc-vct.ts
@@ -1,6 +1,6 @@
 import type { TypeMetadataFormat } from './sd-jwt-vc-type-metadata-format';
 
-export type VcTFetcher = (
+export type VCTFetcher = (
   uri: string,
   integrity?: string,
 ) => Promise<TypeMetadataFormat | undefined>;
